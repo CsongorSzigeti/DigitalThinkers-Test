3a8830d09502f153cc1812f414f9c6d2dc4f30b6
Csongor Szigeti <csongor.szigeti@gmail.com>
2021-01-28 12:46:33 +0100

    Connected the backend with the frontend and created a basic Cocktail Name get endpoint to test it

--- coctail-api/index.js
+++ coctail-api/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const request = require("request");
+
 const app = express();
 const port = 3080;
 
-app.get("/", (req, res) => {
-  res.send("Hello DigitalThinkers!");
+app.get("/api/cocktail", (req, res) => {
+  request(
+    "https://www.thecocktaildb.com/api/json/v1/1/random.php",
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        const parsedBody = JSON.parse(body);
+        const cocktail_name = parsedBody["drinks"][0]["strDrink"];
+        res.send({ cocktail_name });
+      }
+    }
+  );
 });
 
 app.listen(port, () => {
--- coctail-app/src/App.js
+++ coctail-app/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import RandomCocktail from "./components/RandomCocktail";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hey! I'm a React App for DigitalThinkers</h1>
+        <RandomCocktail />
       </div>
     );
   }
